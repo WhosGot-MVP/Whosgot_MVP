40c3c77160aaec99da9fa86d7bea4662f2fb505d
WhosGot <platoxa@yahoo.com>
2025-08-17 17:46:53 +0500

    Create about.js

---
+++ frontend/pages/about.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main style={{padding:24, maxWidth:800, margin:'0 auto'}}>
+      <h1>About WhosGot</h1>
+      <p>A platform where people help each other not for profit, but for human connection.</p>
+      <ul>
+        <li>Post a request.</li>
+        <li>Get responses.</li>
+        <li>Find help or new connections.</li>
+      </ul>
+    </main>
+  );
+}
